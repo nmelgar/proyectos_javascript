e3d9d43ae2d440a8f239a5ae2a2c5462a425210d
nmelgar <nmelgar16013@gmail.com>
2023-08-24 20:12:53 -0600

    Agregada css de todo individual. Agregado todo individual usando js, funciona. La funcion addTodo funciona correctamente.

--- todo_list/script.js
+++ todo_list/script.js
@@ -12,3 +12,37 @@ console.log(actualDate);
 
 let modifyDate = document.getElementById("currente-date");
 modifyDate.textContent = actualDate;
+
+function addTodo() {
+  // parent for the todos (todo individual)
+  let todosContainer = document.getElementById("todos-container");
+
+  //container for individual to do
+  let todoIndividual = document.createElement("div");
+  todoIndividual.id = "todo-individual";
+
+  // first child of individual to do
+  let checkbokInput = document.createElement("input");
+  checkbokInput.type = "checkbox";
+  checkbokInput.id = "todo1";
+  checkbokInput.name = "todo1";
+  checkbokInput.value = "todo1";
+
+  // second child of individual to do
+  let checkbokLabel = document.createElement("label");
+  checkbokLabel.textContent = "todo text";
+  checkbokLabel.setAttribute("for", "todo1");
+
+  // third child of individual to do
+  let todoCategory = document.createElement("p");
+  todoCategory.id = "todo-category";
+  todoCategory.textContent = "category1";
+
+  todosContainer.appendChild(todoIndividual);
+  todoIndividual.appendChild(checkbokInput);
+  todoIndividual.append(checkbokLabel);
+  todoIndividual.appendChild(todoCategory);
+}
+
+addTodo();
+
